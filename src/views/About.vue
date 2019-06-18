524044766c1c3388311c809596a96debc8c79f20
oomusou <oomusou@gmail.com>
2019-06-18 11:28:55 +0800

    Initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>About</h1>
+    <router-link to="/about/us">About Us</router-link> |
+    <router-link to="/about/you">About You</router-link> |
+    <router-link to="/about/both">About Both</router-link>
+    <router-view name="default"></router-view>
+    <router-view name="another"></router-view>
   </div>
 </template>
